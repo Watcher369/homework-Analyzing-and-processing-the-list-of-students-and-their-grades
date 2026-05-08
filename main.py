80222e2059beadf33a708b09054b9068eb71b18b
alyat <black1shooter@gmail.com>
2026-05-08 19:05:53 +0300

    Обновлено домашнее задание 3.0

--- main.py
+++ main.py
@@ -142,7 +142,14 @@ def delete_student_with_lowest_grades(students):
     show_group_average(students)
 
 
-def launch_menu(students):
+def main():
+    students = [
+        {"name": "Гарри", "grades": [80, 90, 78]},
+        {"name": "Гермиона", "grades": [95, 99, 97]},
+        {"name": "Рон", "grades": [60, 70, 64]},
+        {"name": "Драко", "grades": [60, 75, 70]},
+    ]
+
     menu = {
         "1": ["Показать информацию о студентах", show_students_info],
         "2": ["Показать общий средний балл", show_group_average],
@@ -171,4 +178,4 @@ def launch_menu(students):
 
 
 if __name__ == "__main__":
-    launch_menu(students)
+    main()
